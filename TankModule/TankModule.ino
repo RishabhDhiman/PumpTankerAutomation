60989c21c92c5ccdba20e8b979d7be2e71230488
rishabhdhiman <rishabhdhiman12345@gmail.com>
2020-12-14 01:05:29 +0530

    Implemented Calculation of water level and sending appropriate command to pump set module.

--- TankModule/TankModule.ino
+++ TankModule/TankModule.ino
@@ -3,8 +3,18 @@
 #include <RH_NRF24.h>
 #include <LowPower.h>
 #include <EEPROM.h>
+
+/**
+ * MIN_DISTANCE_ADDRESS_1 ,MIN_DISTANCE_ADDRESS_1 referce to the distance of the water from the arduino module. Water cannot rise above this level 
+ * becuase it can touch the Arduino or start flowing from pipe at the top of the tank. This variable is used to store the maximum height of water 
+ * the tank can store.
+ */
 #define MIN_DISTANCE_ADDRESS_1 0
 #define MIN_DISTANCE_ADDRESS_2 1
+
+/**
+ * MAX_DISTANCE_ADDRESS_1 ,MAX_DISTANCE_ADDRESS_2 This variable is used to store the address of the minimum level of water after which the pump must start.
+ */
 #define MAX_DISTANCE_ADDRESS_1 2
 #define MAX_DISTANCE_ADDRESS_2 3
 #define TANK_HEIGHT_ADDRESS_1 4
@@ -17,7 +27,13 @@ int TANK_HEIGHT_1 = 0;
 int TANK_HEIGHT_2 = 0;
 int TANK_HEIGHT = 0;
 int currentWaterLevel = 0;
+/**
+ * MIN_WATER_LEVEL This variable is used to store the minimum level of water after which the pump must start. 
+ */
 int MIN_WATER_LEVEL = 0;
+/**
+ * This variable is used to store the maximum height of water the tank can store.
+ */
 int MAX_WATER_LEVEL = 0;
 RH_NRF24 nrf24;
 struct Data
@@ -43,6 +59,7 @@ void loop()
         d.command = 1;
         d.waterLevel = currentWaterLevel / TANK_HEIGHT;
         sendData(d);
+        Serial.println("Turning Pump On");
     }
     else if (currentWaterLevel >= MAX_WATER_LEVEL)
     {
@@ -50,8 +67,8 @@ void loop()
         d.command = 0;
         d.waterLevel = currentWaterLevel / TANK_HEIGHT;
         sendData(d);
+        Serial.println("Turning Pump Off");
     }
-    Serial.println("Rishabh");
     delay(300);
     for (int i = 0; i < 15; i++)
     {
@@ -77,45 +94,54 @@ void initializeMinMaxDistance()
         EEPROM.write(MIN_DISTANCE_ADDRESS_1, 10);
         EEPROM.write(MIN_DISTANCE_ADDRESS_2, 0);
         MIN_DISTANCE_1 = 10;
-        MIN_DISTANCE_2 = 10;
+        MIN_DISTANCE_2 = 0;
     }
     if (MAX_DISTANCE_1 == 255 && MAX_DISTANCE_2 == 255)
     {
         EEPROM.write(MAX_DISTANCE_ADDRESS_1, 39);
         EEPROM.write(MAX_DISTANCE_ADDRESS_2, 0);
-        MIN_DISTANCE_1 = 39;
-        MIN_DISTANCE_2 = 0;
+        MAX_DISTANCE_1 = 39;
+        MAX_DISTANCE_2 = 0;
     }
     if (TANK_HEIGHT_1 == 255 && TANK_HEIGHT_2 == 255)
     {
-        EEPROM.write(MIN_DISTANCE_ADDRESS_1, 10);
+        EEPROM.write(MIN_DISTANCE_ADDRESS_1, 78);
         EEPROM.write(MIN_DISTANCE_ADDRESS_2, 0);
-        TANK_HEIGHT_1 = 10;
-        TANK_HEIGHT_2 = 10;
+        TANK_HEIGHT_1 = 78;
+        TANK_HEIGHT_2 = 0;
     }
-    MIN_WATER_LEVEL = MIN_DISTANCE_1 + MIN_DISTANCE_2;
-    MAX_WATER_LEVEL = MAX_DISTANCE_1 + MAX_DISTANCE_2;
+    MAX_WATER_LEVEL = MIN_DISTANCE_1 + MIN_DISTANCE_2;
+    MIN_WATER_LEVEL = MAX_DISTANCE_1 + MAX_DISTANCE_2;
     TANK_HEIGHT = TANK_HEIGHT_1 + TANK_HEIGHT_2;
+
+    Serial.print("Min Water Level ");
+    Serial.println(MIN_WATER_LEVEL);
+
+    Serial.print("Max Water Level ");
+    Serial.println(MAX_WATER_LEVEL);
+
+    Serial.print("Tank Height ");
+    Serial.println(TANK_HEIGHT);
 }
 
 void initializeTransciver()
 {
     if (!nrf24.init())
-        Serial.println("init failed");
+        Serial.println("Init Failed");
     else
     {
-        Serial.println("init Success");
+        Serial.println("Init Success");
     }
     // Defaults after init are 2.402 GHz(channel 2), 2Mbps, 0dBm
     if (!nrf24.setChannel(1))
-        Serial.println("setChannel failed");
+        Serial.println("SetChannel Failed");
     else
     {
-        Serial.println("setChannel Succcess");
+        Serial.println("SetChannel Succcess");
     }
 
     if (!nrf24.setRF(RH_NRF24::DataRate250kbps, RH_NRF24::TransmitPowerm20dBm))
-        Serial.println("setRF failed");
+        Serial.println("SetRF Failed");
     else
     {
         Serial.println("Set RF Success");
